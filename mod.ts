0caf67a3e1f1c46b3ea8e5b09124a60dd5ec2991
Abel Masila <abel@x2ai.com>
2022-07-28 02:49:55 +0300

    Added Log and timer middleware

--- mod.ts
+++ mod.ts
@@ -4,8 +4,24 @@ const app = new Application();
 
 const PORT = 8000;
 
-app.use((ctx) => {
+//logger middleware
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get('X-Response-Time');
+
+  console.log(`${ctx.request.method} ${ctx.request.url}- ${time}`);
+});
+//Timing middleware
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set('X-Response-Time', `${delta} ms`);
+});
+
+app.use(async (ctx, next) => {
   ctx.response.body = 'Mission Control API';
+  await next();
 });
 
 if (import.meta.main) {
